893be272f53f40e7efc1106e86e3e098f50ed016
Leechaehyeon1 <lch09253@hanmail.net>
2021-10-17 12:02:43 +0900

    자동 정렬 기능

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 window.addEventListener('load', () => {
-  new Isotope('section', {
+  const gird = new Isotope('section', {
     itemSelector: 'article',
     columnWidth: 'article',
     transitionDuration: '0.5s'
   });
+
+  const btns = document.querySelectorAll('main ul li');
+  for(let elem of btns){
+    elem.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      let target = e.currentTarget.querySelector('a').getAttribute('href');
+      gird.arrange({filter: target});
+
+      for(let elem2 of btns){
+        elem2.classList.remove('on');
+      }
+      elem.classList.add('on');
+    });
+  }
 });
